865b8dc0c9cab1e557bb24e517edfa6ca3dbdec4
Harry Johnston <zcaphjo@ucl.ac.uk>
2016-11-06 19:58:10 +0000

    testing chi2

--- catalog_sampler.py
+++ catalog_sampler.py
@@ -452,7 +452,7 @@ class RealCatalogue:
 		covarData = np.array(['covar' in x for x in filesList])
 		wcorrList = filesList[wcorrData]
 		covarList = filesList[covarData]
-		[print(i,j) for i,j in [wcorrList,covarList]]
+		[print(i,covarList[j]) for j,i in enumerate(wcorrList)]
 		# [print(i) for i in covarList]
 
 		dataArr = np.array([np.loadtxt(join(path2data, i),skiprows=1) for i in wcorrList])
