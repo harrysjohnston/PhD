9513c973a282c7b07dd09b8179465567393d70b2
Harry Johnston <zcaphjo@ucl.ac.uk>
2017-01-12 12:26:45 +0000

    caps

--- extra1.py
+++ extra1.py
@@ -4,7 +4,7 @@ from cosmosis.postprocessing import lazy_pylab as pylab
 class PmIplot(Plot):
     filename='matter_intrinsic_power'
     def plot(self):
-        super(PmIPlot,self).plot()
+        super(PmIplot,self).plot()
         k = self.load_file("matter_intrinsic_power", "k_h")
         p_k = self.load_file("matter_intrinsic_power", "p_k")
         pylab.loglog(k_h, p_k)
