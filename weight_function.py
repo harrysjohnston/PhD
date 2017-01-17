4a90bb8a9f70637aeb8169de13b3a38d36e91135
Harry Johnston <zcaphjo@ucl.ac.uk>
2017-01-17 15:46:10 +0000

    module to compute weight fn W(z) & integrate with wg+(r,z) -> wg+(r)

---
+++ weight_function.py
@@ -0,0 +1,43 @@
+from __future__ import print_function,division
+import numpy as np
+from astropy.cosmology import Planck13
+import scipy.integrate as scint
+import operator
+
+def fivept_stencil(func,x,h):
+    # returns f'(x), via 5pt stencil, for grid-spacing h
+    return (-func(x+2*h)+8*func(x+h)-8*func(x-h)+func(x-2*h))/(12*h)
+
+def compute_Wz(z,*bin_popns):
+    # Wz = [p^2 / X^2*X'] / int[p^2 / X^2*X' dz]
+
+    # compute p(z) = unconditional pdf
+    nofz = sum(bin_popns)
+    print('nofz tuple-sum check: ',nofz.shape,nofz,sum(nofz))
+    pz = nofz/sum(nofz)
+    assert pz.shape==z.shape, "p(z) vs. z mismatch"
+
+    # compute X(z) = comoving coordiante
+    P13comov = lambda x: Planck13.comoving_distance(x)
+    Xz = P13comov(z)
+    Xz2 = Xz**2
+
+    # compute X'(z) = first deriv.
+    h = z[1]-z[0]
+    Xprime = fivept_stencil(P13comov,z,h)
+
+    # combine & integrate (Riemann sum) over z
+    Wz_nom = (pz**2)/(Xz2*Xprime)
+    Wz_dom = sum(Wz_nom)*h
+    Wz = Wz_nom/Wz_dom
+    return Wz # WILL NEED TO EDIT FOR HIGH- & LOW-Z 
+
+def compute_wgp(Wz,wgp_rz):
+    # Riemann sum over W(z) for wgp(r,z) -> wgp(r)
+    for i in range(nbin):
+        wgp_rz[i] *= Wz[i]
+    wgp_r = np.sum(wgp_rz,axis=0)*dz
+    return wgp_r
+
+
+
---
+++ weight_function_interface.py
@@ -0,0 +1,47 @@
+from cosmosis.datablock import names, option_section
+import numpy as np
+import weight_function
+from weight_function import compute_Wz
+from weight_function import compute_wgp
+
+# We have a collection of commonly used pre-defined block section names.
+# If none of the names here is relevant for your calculation you can use any
+# string you want instead.
+cosmo = names.cosmological_parameters
+
+def setup(options):
+    # this function is called ONCE per processor per chain
+    # use to load fixed parameters, perform one-off calculations (i.e. not changing
+    # with sampling) etc.
+
+    nz_section = options.get_string(option_section,'nz_section',default='wl_number_density')
+    nbin = options[option_section,'nbin']
+    wgp_section = options[option_section,'wgp_section']
+    # INCLUDE A ZCUT ARG FOR HIGH- & LOW-Z WEIGHT-FNS
+
+    # return the config for execute fn
+    return nz_section,nbin,wgp_section
+
+def execute(block, config):
+    # this function is called every time you have a new sample of cosmological and other parameters
+
+    nz_section,nbin,wgp_section = config
+
+    # load from block
+    z = block[nz_section,'z']
+    dz = z[1]-z[0]
+    wgp_rz = np.array([block[wgp_section,'bin_%s_%s'%(i+1,i+1)] for i in range(nbin)])
+    bin_popns = tuple([block[nz_section,"bin_%s"%(i+1)] for i in range(nbin)])
+
+    # compute W(z), wgp(r)
+    Wz = compute_Wz(z,bin_popns)
+    wgp_r = compute_wgp(Wz, wgp_rz)
+    block.put_grid(wgp_section,'wgp(r_p)',wgp_r,'W(z)',Wz)
+
+    # return zero == all done, no probs
+    return 0
+
+def cleanup(config):
+    # Usually python modules do not need to do anything here.
+    # We just leave it in out of pedantic completeness.
+    pass
