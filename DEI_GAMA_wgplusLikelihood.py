019a39e5b8bf64b4ee0cb1b3d43347c34183a9bc
Harry Johnston <zcaphjo@ucl.ac.uk>
2017-02-22 14:53:24 +0000

    indentation

--- DEI_GAMA_wgplusLikelihood.py
+++ DEI_GAMA_wgplusLikelihood.py
@@ -9,17 +9,17 @@ class DEI_GAMA_wgplusLikelihood(GaussianLikelihood):
     y_name    = "wgp_r"
     like_name = "IAwgp"
 
-        def build_data(self):
-                #use self.options to find the data_file and load ell, tt from it
-                data_file = self.options.get_string("data_file")
-                print("loading wgp measurement, skipping row=1 (column heads)...!")
-                rp,wgp = np.loadtxt(data_file,skiprows=1).T[:2]
-                return rp,wgp
+    def build_data(self):
+        #use self.options to find the data_file and load ell, tt from it
+        data_file = self.options.get_string("data_file")
+        print("loading wgp measurement, skipping row=1 (column heads)...!")
+        rp,wgp = np.loadtxt(data_file,skiprows=1).T[:2]
+        return rp,wgp
 
-        def build_covariance(self):
-                cov_file = self.options.get_string("cov_file")
-                print("loading covariance, skipping row=1 (column heads)...!")
-                covmat = np.loadtxt(cov_file,skiprows=1)
-                return covmat
+    def build_covariance(self):
+        cov_file = self.options.get_string("cov_file")
+        print("loading covariance, skipping row=1 (column heads)...!")
+        covmat = np.loadtxt(cov_file,skiprows=1)
+        return covmat
 
 setup,execute,cleanup = DEI_GAMA_wgplusLikelihood.build_module()
