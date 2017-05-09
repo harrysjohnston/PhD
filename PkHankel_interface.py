7f49d0d7d9c2bd99ca5b3e614524a5c81cb40212
Harry Johnston <zcaphjo@ucl.ac.uk>
2017-05-09 11:49:46 +0100

    make all functions optional

--- PkHankel_interface.py
+++ PkHankel_interface.py
@@ -14,23 +14,31 @@ def setup(options):
     # use to load fixed parameters, perform one-off calculations (i.e. not changing
     # with sampling) etc.
 
-    power_section = options[option_section,'power_section'] # options[] must be in .ini
-    nbin = options[option_section,'nbin']
-    make_nz = options.get_bool(option_section,'make_nz',default=True)
-    coerce_pk = options.get_bool(option_section,'coerce_pk',default=True)
+    # prep P(k) for hankel transformation
+    coerce_pk = options.get_bool(option_section,'coerce_pk',default=False)
+    power_section = options.get_string(option_section,'power_section',default=None)
+    nbin = options.get_int(option_section,'nbin',default=50)
+
+    # create n(z) files for weight functions
+    make_nz = options.get_bool(option_section,'make_nz',default=False)
     nz_section = options.get_string(option_section,'nz_section',default=None)
     shapz_path = options.get_string(option_section,'shapZ_path',default=None)
     densz_path = options.get_string(option_section,'densZ_path',default=None)
-    nz = options.get_int(option_section,'nz',default=None)
+    nz = options.get_int(option_section,'nz',default=50)
     outfile = options.get_string(option_section,'outfile',default=None)
 
-    # optionally, generate ascii & save info on n(z) into datablock
     if make_nz:
         shapes_z,density_z = read_z(shapz_path,densz_path)
         z_mids,nofz_shap = create_nz(shapes_z,nz,nbin,outfile+'shapes.asc')
         z_mids,nofz_dens = create_nz(density_z,nz,nbin,outfile+'density.asc')
     else:
         z_mids,nofz,nz = (None,None,None)
+
+    if (not coerce_pk) & (not make_nz):
+        print('PKHANKEL IS DOING NOTHING !!')
+        print('PkHankel can (1) coerce P(k) into hankel-transformable (cl_to_xi) format in datablock')
+        print('and/or (2) create 2x n(z) arrays (2 samples) for computing weight fns')
+
 
     # return the config for execute fn
     return (power_section,nbin,make_nz,coerce_pk,z_mids,nofz_shap,nofz_dens,nz_section,nz)
@@ -41,6 +49,7 @@ def execute(block, config):
     power_section,nbin,make_nz,coerce_pk,z_mids,nofz_shap,nofz_dens,nz_section,nz = config
 
     if coerce_pk:
+        print('PkHankel: readying P(k) for hankel transform..')
         # load from datablock
         k_h = block[power_section,'k_h']
         p_k = block[power_section,'p_k']
@@ -55,6 +64,7 @@ def execute(block, config):
             block[power_section,'bin_%s_%s'%(i+1,i+1)] = p_k[i]
 
     if make_nz:
+        print('PkHankel: making n(z) arrays for weight functions..')
         block.put_double_array_1d(nz_section,'z',z_mids)
         block.put_int(nz_section,'nz',nz)
         block.put_int(nz_section,'nbin',nbin)
