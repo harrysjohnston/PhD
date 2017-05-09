239f4e465c817d33b56305815ce875d6341b24a1
Harry Johnston <zcaphjo@ucl.ac.uk>
2017-05-09 11:13:51 +0100

    make all functions optional

--- PkHankel_interface.py
+++ PkHankel_interface.py
@@ -17,6 +17,7 @@ def setup(options):
     power_section = options[option_section,'power_section'] # options[] must be in .ini
     nbin = options[option_section,'nbin']
     make_nz = options.get_bool(option_section,'make_nz',default=True)
+    coerce_pk = options.get_bool(option_section,'coerce_pk',default=True)
     nz_section = options.get_string(option_section,'nz_section',default=None)
     shapz_path = options.get_string(option_section,'shapZ_path',default=None)
     densz_path = options.get_string(option_section,'densZ_path',default=None)
@@ -32,25 +33,26 @@ def setup(options):
         z_mids,nofz,nz = (None,None,None)
 
     # return the config for execute fn
-    return (power_section,nbin,make_nz,z_mids,nofz_shap,nofz_dens,nz_section,nz)
+    return (power_section,nbin,make_nz,coerce_pk,z_mids,nofz_shap,nofz_dens,nz_section,nz)
 
 def execute(block, config):
     # this function is called every time you have a new sample of cosmological and other parameters
     # collect config variables
-    power_section,nbin,make_nz,z_mids,nofz_shap,nofz_dens,nz_section,nz = config
+    power_section,nbin,make_nz,coerce_pk,z_mids,nofz_shap,nofz_dens,nz_section,nz = config
 
-    # load from datablock
-    k_h = block[power_section,'k_h']
-    p_k = block[power_section,'p_k']
+    if coerce_pk:
+        # load from datablock
+        k_h = block[power_section,'k_h']
+        p_k = block[power_section,'p_k']
 
-    # execute main function
-    # aim for simplicity
-    # here, cutting k,P(k) & saving back to db in format for Hankel transfm
-    k_h,p_k = cut_krange(k_h,p_k, kmin=10**-2.2, kmax=10**1.2) # k-limits in h/Mpc
-    block[power_section,'ell'] = k_h
-    block[power_section,'nbin'] = nbin
-    for i in range(len(p_k)):
-        block[power_section,'bin_%s_%s'%(i+1,i+1)] = p_k[i]
+        # execute main function
+        # aim for simplicity
+        # here, cutting k,P(k) & saving back to db in format for Hankel transfm
+        k_h,p_k = cut_krange(k_h,p_k, kmin=10**-2.2, kmax=10**1.2) # k-limits in h/Mpc
+        block[power_section,'ell'] = k_h
+        block[power_section,'nbin'] = nbin
+        for i in range(len(p_k)):
+            block[power_section,'bin_%s_%s'%(i+1,i+1)] = p_k[i]
 
     if make_nz:
         block.put_double_array_1d(nz_section,'z',z_mids)
