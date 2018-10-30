a70924327d8f840c7ce5e6887a6cd84984944a4e
Harry Johnston <zcaphjo@ucl.ac.uk>
2018-10-30 13:08:33 +0000

    RRs pairs from clustering -- needs testing

--- treecorr_3DCF.py
+++ treecorr_3DCF.py
@@ -1,11 +1,11 @@
 from functions import *
 import treecorr
 
-def treecorr_3DCF(dataf, randf, config, estimator, nbins_rpar=30):
+def compute_w(dataf, randf, config, estimator, nbins_rpar=30):
 	"""
 	dataf: paths to galaxy samples
 	randf: paths to random points corresponding to galaxy samples
-	config: dict specifying file types, column names/numbers, etc. a la TreeCorr configuration
+	config: path to config file, or dict specifying file types; column names/numbers, etc. a la TreeCorr configuration
 	estimator: 'PW', 'AS' or 'wgg' to specify correlation & estimator
 	nbins_rpar: number of line-of-sight bins for 3D correlation function -- specify limits in config arg
 	"""
@@ -13,17 +13,18 @@ def treecorr_3DCF(dataf, randf, config, estimator, nbins_rpar=30):
 	assert hasattr(dataf, '__iter__'), "dataf must be list/tuple of 2x paths; density, shapes for IA, or density1, density2 for clustering (these can be the same!)"
 	assert hasattr(randf, '__iter__'), "randf must be list/tuple of 2x paths for randoms corresponding to each of dataf (these can be the same!)"
 
+	if type(config) == str:
+		config = treecorr.read_config(config)
+
 	if estimator in ['PW', 'AS']:
 		corr = 'ng'
-		XXCorrelation = treecorr.NGCorrelation
 		gt_3D = np.zeros([len(Pi)-1, config['nbins']])
 		gx_3D = np.zeros([len(Pi)-1, config['nbins']])
 	elif estimator == 'wgg':
 		corr = 'nn'
-		XXCorrelation = treecorr.NNCorrelation
 		wgg_3D = np.zeros([len(Pi)-1, config['nbins']])
 	else:
-		raise ValueError, "something went wrong with treecorr_3D!"
+		raise ValueError, "unsupported estimator choice"
 
 	Pi = np.linspace(config['min_rpar'], config['max_rpar'], nbins_rpar + 1)
 	config_r = config.copy()
@@ -40,27 +41,25 @@ def treecorr_3DCF(dataf, randf, config, estimator, nbins_rpar=30):
 
 		# HOW to handle shot-noise errors?
 		if corr == 'ng':
-			ng = XXCorrelation(conf_pi)
-			rg = XXCorrelation(conf_pi)
+			ng = NGCorrelation(conf_pi)
+			rg = NGCorrelation(conf_pi)
 			ng.process_cross(data1, data2)
 			rg.process_cross(rand1, data2)
 
 			if estimator == 'PW':
-				# these are RD_s pairs, not R_sR
-				# may need to get R_sR from clustering...
-				norm = rg.weight * float(data2.ntot) / float(rand1.ntot)
+				norm = rg.weight * data1.ntot / float(rand1.ntot) # RDs
+				#norm = get_RRs(rand1, rand2, conf_pi) * data1.ntot * data2.ntot / float(rand1.ntot * rand2.ntot) # RRs
 			elif estimator == 'AS':
-				# norm by D_sD pairs
-				norm = ng.weight
+				norm = ng.weight # DDs
 
-			gt_3D += -ng.xi / norm
-			gx_3D += -ng.xi_im / norm
+			gt_3D[p] += -ng.xi / norm
+			gx_3D[p] += -ng.xi_im / norm
 
-		elif corr == 'gg':
-			nn = XXCorrelation(conf_pi)
-			rr = XXCorrelation(conf_pi)
-			nr = XXCorrelation(conf_pi)
-			rn = XXCorrelation(conf_pi)
+		elif corr == 'nn':
+			nn = NNCorrelation(conf_pi)
+			rr = NNCorrelation(conf_pi)
+			nr = NNCorrelation(conf_pi)
+			rn = NNCorrelation(conf_pi)
 
 			if dataf[0] == dataf[1]:
 				nn.process(data1)
@@ -86,7 +85,10 @@ def treecorr_3DCF(dataf, randf, config, estimator, nbins_rpar=30):
 		r = nn.rnom
 		return r, wgg
 
-
+def get_RRs(R_cat, Rs_cat, config):
+	rrs = treecorr.NNCorrelation(config)
+	rrs.process_cross(R_cat, Rs_cat)
+	return rrs.weight
 
 
 
